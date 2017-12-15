9816d576790bb1c1b4987cd7cf5e46fc5a3adda7
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2017-12-15 12:16:48 +1100

    Added IMessageToSubscribersEntityFactory

---
+++ gcc/in_memory_bus/InMemoryBus/src/InMemoryBus/Subscribtions/MessageToSubscribers/IMessageToSubscribersEntityFactory.h
@@ -0,0 +1,28 @@
+/*
+ * IMessageToSubscribersEntityFactory.h
+ *
+ *  Created on: 15 Dec. 2017
+ *      Author: tom
+ */
+
+#ifndef INMEMORYBUS_SUBSCRIBTIONS_MESSAGETOSUBSCRIBERS_IMESSAGETOSUBSCRIBERSENTITYFACTORY_H_
+#define INMEMORYBUS_SUBSCRIBTIONS_MESSAGETOSUBSCRIBERS_IMESSAGETOSUBSCRIBERSENTITYFACTORY_H_
+
+#include "IMessageToSubscribersEntity.h"
+
+namespace InMemoryBus {
+namespace Subscribtions {
+namespace MessageToSubscribers {
+class IMessageToSubscribersEntityFactory {
+ public:
+  virtual ~IMessageToSubscribersEntityFactory() = default;
+
+  virtual IMessageToSubscribersEntity_SPtr create(const string message_type) = 0;
+};
+}
+}
+}
+
+typedef std::shared_ptr<InMemoryBus::Subscribtions::MessageToSubscribers::IMessageToSubscribersEntityFactory> IMessageToSubscribersEntityFactory_SPtr;
+
+#endif /* INMEMORYBUS_SUBSCRIBTIONS_MESSAGETOSUBSCRIBERS_IMESSAGETOSUBSCRIBERSENTITYFACTORY_H_ */
