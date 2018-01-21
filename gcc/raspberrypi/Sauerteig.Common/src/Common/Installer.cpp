8af0b8bc4fabd3b5c7b5b75da2b5dc623b77da02
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2018-01-22 08:12:37 +1100

    Improved Logger

--- gcc/raspberrypi/Sauerteig.Common/src/Common/Installer.cpp
+++ gcc/raspberrypi/Sauerteig.Common/src/Common/Installer.cpp
@@ -10,12 +10,10 @@
 #include "Hypodermic/ContainerBuilder.h"
 #include "Logger.h"
 #include "Interfaces/ILogger.h"
-#include "NanoLog/NanoLogLogger.h"
 
 using namespace Common;
 
 void Installer::register_components(Hypodermic::ContainerBuilder& builder) {
-    builder.registerType<Logger>();
-    builder.registerType<NanoLog::NanoLogLogger>().as<Interfaces::ILogger>().singleInstance();
+    builder.registerType<Logger>().as<Interfaces::ILogger>();
 }
 
