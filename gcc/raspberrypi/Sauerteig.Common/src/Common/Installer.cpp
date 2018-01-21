39b08f9c257ff2b6194940845f0ca45a7cc8707a
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2018-01-21 21:24:13 +1100

    WIP: NanoLog

--- gcc/raspberrypi/Sauerteig.Common/src/Common/Installer.cpp
+++ gcc/raspberrypi/Sauerteig.Common/src/Common/Installer.cpp
@@ -10,10 +10,12 @@
 #include "Hypodermic/ContainerBuilder.h"
 #include "Logger.h"
 #include "Interfaces/ILogger.h"
+#include "NanoLog/NanoLogLogger.h"
 
 using namespace Common;
 
 void Installer::register_components(Hypodermic::ContainerBuilder& builder) {
-    builder.registerType<Logger>().as<Interfaces::ILogger>();
+    builder.registerType<Logger>();
+    builder.registerType<NanoLog::NanoLogLogger>().as<Interfaces::ILogger>().singleInstance();
 }
 
